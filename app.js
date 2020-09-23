8ff23f8b06e0cf587dd3f2b37d7b963ca2f81601
Bruna Bornancin <67303988+brunabornancin@users.noreply.github.com>
2020-09-23 14:26:20 -0600

    Updated

--- app.js
+++ app.js
@@ -5,10 +5,27 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/gb_yyc', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('db connected.');
+});
+
+var basicRouter = require('./routes/basicRoutes');
+var prospectsRouter = require('./routes/prospects');
+var Article = require('./models/article')
+var articleRouter = require('./routes/articles');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,8 +43,14 @@ app.use(sassMiddleware({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/articles/', async (req, res) => {
+	var articles = await Article.find().sort({createdAt: 'desc'})
+	res.render('articles/index', {articles: articles})
+});
+
+app.use('/', basicRouter);
+app.use('/', prospectsRouter);
+app.use('/', articleRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
